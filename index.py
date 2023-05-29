e890101d8bf67b46f9a42354cd02d26ee15d4ef1
xyylkk6202 <134909524+xyylkk6202@users.noreply.github.com>
2023-05-29 15:03:29 +0800

    Update index.py

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ from linebot.models import (
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('pvdnxLXOCqG9kV14KT10JeHe+aDUe+HNacCWbbNbVr/cPJHdbxsDVsDeAFZNl9bKB2wqpqnZZeVEWCwZ2K8IKnerrmwA+v1OR5LZTF1AgGh41jxuj6fyx25M8lsqG/UhwKA7trbH5FNUQ6tWhQ9vfQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('e0761cc5c843292fdc4edef2df344ab7')
+handler1 = WebhookHandler('e0761cc5c843292fdc4edef2df344ab7')
 
 
 @app.route("/callback", methods=['POST'])
@@ -27,7 +27,7 @@ def callback():
 
     # handle webhook body
     try:
-        handler.handle(body, signature)
+        handler1.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
@@ -36,52 +36,6 @@ def callback():
 
 
 @handler1.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
-    app.run()
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
